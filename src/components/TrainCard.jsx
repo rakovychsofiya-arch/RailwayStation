ab8a0c1ae21da0284a7c785380709b21052e31f3
Sofiia Rakovych <-email---github@example.com>
2026-05-06 00:00:59 +0300

    Add missing display departure and arrival dates in TrainCard

--- src/components/TrainCard.jsx
+++ src/components/TrainCard.jsx
@@ -5,6 +5,12 @@ const TrainCard=({train})=>{
         const m = minutes % 60;
         return `${h} г  ${m} хв `
     };
+const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleDateString("uk-UA", {
+      day: "numeric",
+      month: "long",
+    });
+  };
 const minPrice = Math.min(...train.wagons.map(w => w.price));
 return (
     <div className="train-card">
@@ -19,8 +25,10 @@ return (
 
       <div className="train-info">
         <div className="time-block">
-          <p>Відправлення: <strong>{new Date(train.departure).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</strong></p>
-          <p>Прибуття: {new Date(train.arrival).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+          <p>Відправлення:<strong>{formatDate(train.departure)}</strong> о{" "} 
+           <strong>{new Date(train.departure).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</strong></p>
+          <p>Прибуття: {formatDate(train.arrival)} о{" "}
+            {new Date(train.arrival).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
         </div>
         <p className="duration">У дорозі: {formatDuration(train.durationMinutes)}</p>
       </div>
