b48c86171191a7e576de9961ed986a4a768d85cd
Sofiia Rakovych <-email---github@example.com>
2026-05-05 19:12:24 +0300

    Add toQuery state and input  destination city filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import TrainList from '../components/TrainList';
 const Home = () => {
     //Розділяємо на різні стани (куди та звідки)
     const [fromQuery, setFromQuery] = useState('');
+    const [toQuery, setToQuery] = useState('');
     const filteredTrains = trainsData.filter(train => {
     const matchesFrom = train.route.from.toLowerCase().includes(fromQuery.toLowerCase());
-    return matchesFrom;
+    const matchesTo = train.route.to.toLowerCase().includes(toQuery.toLowerCase());
+    return matchesFrom && matchesTo;
     });
     return (
          <div className="home-page">
@@ -14,11 +16,18 @@ const Home = () => {
       
              <div className="search-container">
                  <input 
-             type="text" 
-             placeholder="Звідки..." 
-             value={fromQuery}
-             onChange={(e) => setFromQuery(e.target.value)}
-             className="search-input"
+                  type="text" 
+                  placeholder="Звідки..." 
+                  value={fromQuery}
+                  onChange={(e) => setFromQuery(e.target.value)}
+                  className="search-input"
+                 />
+                 <input 
+                 type="text" 
+                 placeholder="Куди..." 
+                 value={toQuery}
+                 onChange={(e) => setToQuery(e.target.value)}
+                 className="search-input"
                  />
              </div>  
              <TrainList trains={filteredTrains} />
